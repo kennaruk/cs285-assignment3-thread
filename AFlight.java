01583715fabd18dae9d363c0e178a118a25e7f19
Nitipat Wuttisasiwat <k-e-n_ken@hotmail.com>
2018-11-28 18:56:49 +0700

    synchronous block

--- AFlight.java
+++ AFlight.java
@@ -38,63 +38,69 @@ public class AFlight {
 		return flightNO;
 	}
 
-	public synchronized void reserveSeatForPassenger(String passengerName, String flightNO, String seatNO) {
+	public void reserveSeatForPassenger(String passengerName, String flightNO, String seatNO) {
 
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
 
 		int tryToReserveCount = 1;
 		do {
-			if (this.flightNO.equals(flightNO) && seats.get(seatNO).equals("NOT SPECIFIED")) {
-				try {
-					System.out.println(Thread.currentThread().getName() + " is reserving seat" + seatNO + " at"
-							+ sdf.format(new Date()));
-
-					Thread.sleep((int) Math.random() * 100);
-
-					seats.put(seatNO, passengerName);
-
-					System.out.println(Thread.currentThread().getName() + " finally got " + seatNO + " for "
-							+ seats.get(seatNO) + " at" + sdf.format(new Date()));
-					break;
-				} catch (InterruptedException ex) {
-					Logger.getLogger(AFlight.class.getName()).log(Level.SEVERE, null, ex);
-				}
-			} else {
-				if (tryToReserveCount++ <= 10) {
+			synchronized(this) {
+				if (this.flightNO.equals(flightNO) && seats.get(seatNO).equals("NOT SPECIFIED")) {
 					try {
-//						TimeUnit.SECONDS.sleep(1);
-						wait(1000);
-						continue;
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
+						System.out.println(Thread.currentThread().getName() + " is reserving seat" + seatNO + " at"
+								+ sdf.format(new Date()));
 
-				System.out.println(Thread.currentThread().getName() + " failed to reserve seat" + seatNO + " at"
-						+ sdf.format(new Date()) + " since Seat: " + seatNO + " already reserved for "
-						+ seats.get(seatNO));
-				break;
-			}
+						Thread.sleep((int) Math.random() * 100);
+
+						seats.put(seatNO, passengerName);
+
+						System.out.println(Thread.currentThread().getName() + " finally got " + seatNO + " for "
+								+ seats.get(seatNO) + " at" + sdf.format(new Date()));
+						break;
+					} catch (InterruptedException ex) {
+						Logger.getLogger(AFlight.class.getName()).log(Level.SEVERE, null, ex);
+					}
+				} else {
+					if (tryToReserveCount++ <= 10) {
+						try {
+	//						TimeUnit.SECONDS.sleep(1);
+							wait(1000);
+							continue;
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+
+					System.out.println(Thread.currentThread().getName() + " failed to reserve seat" + seatNO + " at"
+							+ sdf.format(new Date()) + " since Seat: " + seatNO + " already reserved for "
+							+ seats.get(seatNO));
+					break;
+				}
+			}			
 		} while (true);
-		notifyAll();
 	}
 
-	public synchronized void cancelSeatForPassenger(String passengerName, String flightNO, String seatNO) {
+	public void cancelSeatForPassenger(String passengerName, String flightNO, String seatNO) {
 
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
 
-		if (this.flightNO.equals(flightNO) && !seats.get(seatNO).equals("NOT SPECIFIED")) {
-			System.out.println(
-					Thread.currentThread().getName() + " is canceling seat" + seatNO + " at" + sdf.format(new Date()));
+		synchronized(this) {
+			if (this.flightNO.equals(flightNO) && !seats.get(seatNO).equals("NOT SPECIFIED")) {
+				System.out.println(
+						Thread.currentThread().getName() + " is canceling seat" + seatNO + " at" + sdf.format(new Date()));
 
-			seats.put(seatNO, passengerName);
+				seats.put(seatNO, passengerName);
 
-			System.out.println(Thread.currentThread().getName() + " finally cancel " + " at" + sdf.format(new Date()));
-		} else {
-			System.out.println(Thread.currentThread().getName() + " failed to cancel seat" + seatNO + " at"
-					+ sdf.format(new Date()) + " since Seat: " + seatNO + " already not reserved for "
-					+ seats.get(seatNO));
+				System.out.println(Thread.currentThread().getName() + " finally cancel " + " at" + sdf.format(new Date()));
+				notifyAll();
+
+			} else {
+				System.out.println(Thread.currentThread().getName() + " failed to cancel seat" + seatNO + " at"
+						+ sdf.format(new Date()) + " since Seat: " + seatNO + " already not reserved for "
+						+ seats.get(seatNO));
+			}
 		}
+		
 	}
 
 	public void showSeatsStatus() {
